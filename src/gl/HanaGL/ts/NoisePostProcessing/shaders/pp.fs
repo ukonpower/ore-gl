02d767bd0bff04e80bc49b9721aa6e59aeb46f25
ukonpower <obshi0120@icloud.com>
2020-01-14 11:04:38 +0900

    update noise

--- src/gl/HanaGL/ts/NoisePostProcessing/shaders/pp.fs
+++ src/gl/HanaGL/ts/NoisePostProcessing/shaders/pp.fs
@@ -10,37 +10,44 @@ $noise3D
 
 void main(void){
 	vec3 c = vec3(0.0);
-    vec2 u = vUv * 2.0 - 1.0;
-    vec2 uv = vUv;
+	vec2 u = vUv * 2.0 - 1.0;
+	vec2 uv = vUv;
 
-    float w = max(.0,length(u)) * 0.04;
-    
-    vec2 vig = u * w;
+	float w = max(.0,length(u)) * 0.04;
+	
+	vec2 vig = u * w;
 
-    float rad = atan( u.y, u.x );
-    vec2 dir = vec2( cos(rad), sin(rad)) * 0.02 * length( u );
+	float rad = atan( u.y, u.x );
+	vec2 dir = vec2( cos(rad), sin(rad)) * 0.02 * length( u );
 
-    float rnd = random(uv + random(vec2(time)));
+	float rnd = random(uv + random(vec2(time)));
 
-    if(nw > 0.01){
-        // uv.x += sin(uv.y * 500.0) * nw;
-        vec2 n = vec2( random( vec2( time ) ) - 0.5, random( vec2( time + 20.0 ) ) - 0.5 );
-        uv += n * 0.5;
+	float noise = 0.0;
 
-        uv.x += step(0.5,snoise(vec3(uv.y * 2.0,uv.y * 2.0,time * 10.0))) * 0.1;
+	if(nw > 0.1){
+	// if(true){
+		uv.x += sin(uv.y * 500.0) * nw;
+		vec2 n = vec2( random( vec2( time ) ) - 0.5, random( vec2( time + 20.0 ) ) - 0.5 );
+		uv += n * 0.1;
 
-        c += step(0.5,snoise(vec3(uv.y * 2.0 + 100.,uv.y * 2.0,time * 10.0)));
-    }
+		noise = step(0.5,snoise(vec3(uv.y * 2.0,uv.y * 2.0,time * 10.0))) * 0.1;
 
-    for(int i = 0; i < N; i++){
+		uv.x += noise * 0.3;
 
-        vec2 v = vig *( 1.0 + float(i) * 0.02);
+		c += step(0.5,snoise(vec3(uv.y * 2.0 + 100.,uv.y * 2.0,time * 10.0)));
+		
+	}
 
-        c.x += texture2D(backbuffer, uv - ( v + dir * ( 1.0 ) ) ).x;
-        c.y += texture2D(backbuffer, uv - ( v + dir * ( 1.1 ) ) ).y;
-        c.z += texture2D(backbuffer, uv - ( v + dir * ( 1.2 ) ) ).z;
-    }
-    c /= float(N) - 1.0;
+	for(int i = 0; i < N; i++){
+
+		vec2 v = vig *( 1.0 + float(i) * 0.02);
+
+		c.x += texture2D(backbuffer, uv - ( v + dir * ( 1.0 ) ) + vec2( noise, 0.0 ) ).x;
+		c.y += texture2D(backbuffer, uv - ( v + dir * ( 1.1 ) ) ).y;
+		c.z += texture2D(backbuffer, uv - ( v + dir * ( 1.2 ) ) ).z;
+		
+	}
+	c /= float(N) - 1.0;
 
 	c += rnd * 0.1;
 	gl_FragColor = vec4(c,1.0);
